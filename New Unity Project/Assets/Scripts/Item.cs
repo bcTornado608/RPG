f46bba7915e4050fefdfa0c7f1ab2a20e7ea0410
Toby Yang <tianer2820@163.com>
2021-05-18 09:17:05 +0800

    added item class

---
+++ New Unity Project/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The base class for Items
+public abstract class Item
+{
+public static string itemId;
+
+public string itemName;
+
+
+}
