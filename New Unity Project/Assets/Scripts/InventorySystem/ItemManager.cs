1a2c7aab58a4756c3d63ec50881768e120e674ac
Toby Yang <tianer2820@163.com>
2021-05-27 13:59:27 +0800

    switch to dictionary for item data lookup

--- New Unity Project/Assets/Scripts/InventorySystem/ItemManager.cs
+++ New Unity Project/Assets/Scripts/InventorySystem/ItemManager.cs
@@ -6,21 +6,28 @@ public class ItemManager
 {
     public static ItemManager Instance { get; private set; }
 
-    private ItemInfoCollection itemInfoCollection;
+    private Dictionary<string, ItemInfo> itemDictionary;
 
     private ItemManager()
     {
         // read string
         string jsonStr = "";
         TextAsset txt = Resources.Load<TextAsset>("Items/sampleJson");
-
         jsonStr = txt.ToString();
 
-        // create json objects
-        this.itemInfoCollection = JsonUtility.FromJson<ItemInfoCollection>(jsonStr);
+        // create json object
+        ItemInfoCollection itemInfoCollection;
+        itemInfoCollection = JsonUtility.FromJson<ItemInfoCollection>(jsonStr);
+
+        // build lookup table
+        itemDictionary = new Dictionary<string, ItemInfo>();
+        foreach(ItemInfo info in itemInfoCollection.itemInfos)
+        {
+            itemDictionary.Add(info.id, info);
+        }
 
         // test
-        foreach(ItemInfo info in this.itemInfoCollection.itemInfos)
+        foreach(ItemInfo info in itemDictionary.Values)
         {
             Debug.Log(info.name);
         }
