ad8e6f67d4f674f70ebd4aa83b2d1413a1b03cc9
bcTornado608 <82969027+bcTornado608@users.noreply.github.com>
2021-05-28 18:33:25 -0700

    Update BattleSystem -- weapons, mobs, bosses

---
+++ New Unity Project/Assets/Scripts/Battle/DamageCalc.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class DamageCalc
+{
+    public static void Enemy()
+    {
+
+    }
+
+    public static void Ally()
+    {
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Battle/bosses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class boss
+{
+    public string ID;
+    public int HP;
+    public int PhysicalRes;
+    public int MagicalRes;
+    public int Damage;
+    public int Target;
+    public string Ult1Name;
+    public int Ult1Damage;
+    public int Ult1Target;
+    public int Ult1Status;
+    public int Ult1Buff;
+}
+
+[Serializable]
+public class bosses
+{
+    public boss[] Bosses;
+}
